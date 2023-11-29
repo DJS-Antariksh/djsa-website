cf1c87f8bc4db8d5aa83ae6412e0d4aa8c370fbf
Rihansh <rihanshhingad113@gmail.com>
2023-11-29 17:52:55 +0530

    Slider Changed

--- src/components/Achievements/Achievements.jsx
+++ src/components/Achievements/Achievements.jsx
@@ -3,6 +3,7 @@ import { useRef, useEffect, useState } from "react";
 import videoBg from "../../assets/Announcement.mp4";
 import { Player, ControlBar, VolumeMenuButton } from 'video-react';
 import {  useDragControls } from "framer-motion";
+import Carousel from "./Carousel"
 
 
 const Competitions = () => {
@@ -28,7 +29,7 @@ const Competitions = () => {
     }
   };
   return (
-    <div className="mt-14" id="achievements">
+    <div className="mt-14 mb-[10rem]" id="achievements">
       {isVideoPlaying ? (
         <div className="w-screen h-screen">
           <video
@@ -41,125 +42,11 @@ const Competitions = () => {
         ></video>
         </div>
       ) : (
-        <HorizontalScrollCarousel />
+        
+        <Carousel />
       )}
     </div>
   );
 };
 
 export default Competitions;
-
-const HorizontalScrollCarousel = () => {
-  const carousel = useRef();
-  const [width, setwidth] = useState(5)
-  useEffect(() => {
-    const current = carousel.current;
-    if (current) {
-      const calculatedWidth = current.scrollWidth + current.offsetWidth;
-      setwidth(calculatedWidth);
-    }
-  }, []);
-  
-  return (
-    <div id="achievements" className="mt-[4rem] mb-[8rem]">
-      <div className=" justify-center text-center text-3xl text-white font-meth component_title mb-[-5rem]">
-        Achievements
-      </div>
-      <section ref={carousel} className="relative h-[80vh]">
-        <div className="sticky top-0 flex items-center h-screen overflow-hidden">
-          <motion.div
-          className="flex gap-4 inner-corousel"
-          drag='x'
-          dragConstraints={{right:0,left:-width}}
-          whileTap={{cursor:"grabbing"}}
-          >
-            {cards.map((card) => {
-              return <Card card={card} key={card.id} />;
-            })}
-          </motion.div>
-        </div>
-      </section>
-    </div>
-  );
-};
-const cards = [
-  {
-    url: require("../../assets/ERCremote_2023.jpeg"),
-    title: "2nd rank ERC Remote 2023",
-    id: 1,
-  },
-  {
-    url: require("../../assets/ERConsite_2023.jpeg"),
-    title: "10th rank ERC Onsite 2023",
-    id: 2,
-  },
-  {
-    url: require("../../assets/irc-2023.jpg"),
-    title: "8th rank IRC'2023",
-    id: 3,
-  },
-  {
-    url: require("../../assets/IRDC.jpeg"),
-    title: "3rd rank IRDC 2022",
-    id: 4,
-  },
-  {
-    url: require("../../assets/erc_remote_2022.JPG"),
-    title: "1st rank ERC Remote 2022",
-    id: 5,
-  },
-  {
-    url: require("../../assets/irc-2023.jpg"),
-    title: "1st rank ERC Remote 2021",
-    id: 6,
-  },
-  {
-    url: require("../../assets/2nd_IPASC.jpeg"),
-    title: "2nd rank IPASC 2021",
-    id: 7,
-  },
-  {
-    url: require("../../assets/MarsHackathon.jpeg"),
-    title: "8th International Mars hackathon 2020",
-    id: 8,
-  },
-  {
-    url: require("../../assets/erc_remote_3rrd.jpg"),
-    title: "3rd rank ERC remote 2020",
-    id: 9,
-  },
-];
-
-const Card = ({ card }) => {
-  return (
-    <>
-      <div
-        key={card.id}
-        className="group mr-14 relative h-[25rem] w-[25rem] overflow-hidden bg-neutral-200"
-      >
-        <div
-          style={{
-            width: "100%", // Set the width to 100% of the container
-            height: "100%", // Set the height to 100% of the container
-          }}
-          className="absolute inset-0 z-0 transition-transform duration-300 group-hover:scale-110"
-        >
-          <img
-            src={card.url}
-            alt={card.title}
-            style={{
-              width: "100%", // Set the width to 100% of the container
-              height: "100%", // Set the height to 100% of the container
-              objectFit: "cover", // Ensure the image covers the container
-            }}
-          />
-        </div>
-        <div className="absolute inset-0 z-10 grid place-content-center">
-          <p className="p-8 text-3xl font-black text-white uppercase bg-gradient-to-br from-white/20 to-white/0 backdrop-blur-sm">
-            {card.title}
-          </p>
-        </div>
-      </div>
-    </>
-  );
-};
---
+++ src/components/Achievements/Carousel.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import Carousel , { consts } from "react-elastic-carousel";
+import "./Carousel.css"
+import { FaArrowCircleRight , FaArrowCircleLeft } from "react-icons/fa";
+
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2 },
+  { width: 1500, itemsToShow: 3 },
+];
+
+const myArrow = ({ type, onClick, isEdge }) => {
+  const pointer = type === consts.PREV ? <FaArrowCircleRight className='custom_icon' />  : <FaArrowCircleLeft className='custom_icon' />
+  return (
+    <button onClick={onClick} disabled={isEdge}>
+      {pointer}
+    </button>
+  )
+}
+
+const CarouselMain = () => {
+  const settings = {
+    pagination: true,
+    isRTL: true,
+  };
+
+  const items = [
+    
+    {
+      img: require("../../assets/erc_remote_3rrd.jpg"),
+      title: "3rd rank ERC remote 2020",
+      id: 1,
+    },
+    {
+      img: require("../../assets/MarsHackathon.jpeg"),
+      title: "8th International Mars hackathon 2020",
+      id: 2,
+    },
+    {
+      img: require("../../assets/2nd_IPASC.jpeg"),
+      title: "2nd rank IPASC 2021",
+      id: 3,
+    },
+    {
+      img: require("../../assets/irc-2023.jpg"),
+      title: "1st rank ERC Remote 2021",
+      id: 4,
+    },
+    {
+      img: require("../../assets/erc_remote_2022.JPG"),
+      title: "1st rank ERC Remote 2022",
+      id: 5,
+    },
+    {
+      img: require("../../assets/IRDC.jpeg"),
+      title: "3rd rank IRDC 2022",
+      id: 6,
+    },
+    {
+      img: require("../../assets/irc-2023.jpg"),
+      title: "8th rank IRC'2023",
+      id: 7,
+    },
+    {
+      img: require("../../assets/ERConsite_2023.jpeg"),
+      title: "10th rank ERC Onsite 2023",
+      id: 8,
+    },
+    {
+      img: require("../../assets/ERCremote_2023.jpeg"),
+      title: "2nd rank ERC Remote 2023",
+      id: 9,
+    },
+    
+  ];
+  return (
+    <>
+      <h1 className='justify-center text-center text-3xl text-white font-meth component_title mb-[5rem]'>ACHIEVEMENTS</h1>
+      <div className="carousel_elastic ">
+        <Carousel renderArrow = {myArrow} {...settings} breakPoints={breakPoints}>
+          {items.map((item) => (
+            <div className='item_set' key={item.id}>
+              <img className='item_img' src={item.img} alt="" />
+              <h2 className='item_head text-white font-poppins'>{item.title}</h2>
+            </div>
+          ))}
+        </Carousel>
+      </div>
+    </>
+  )
+}
+
+export default CarouselMain
