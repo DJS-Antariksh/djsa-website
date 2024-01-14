d2f2eb2662da12d774be830dfbaad711e95f9111
Rihansh <rihanshhingad113@gmail.com>
2024-01-14 19:57:49 +0530

    IRC

--- src/components/IRC/irc.jsx
+++ src/components/IRC/irc.jsx
@@ -23,16 +23,7 @@ const Irc = () => {
             IRC
           </div>
           <div className="flex flex-row justify-center m-5 mt-16  font-light text-white lg:m-20 font-poppins align-items md:m-10 sm:m-5 sm:my-10">
-            DJS Antariksh is the official Rover challenge team of Dwarkadas J.
-            Sanghvi College of Engineering, Mumbai, India. It was founded in the
-            year 2019-20. The team comprises 100+ passionate undergraduate
-            students from various domains of engineering, working together to
-            explore rover technologies. The team's motto, 'To Decipher
-            Unimaginable', embodies its commitment to space exploration. Its
-            vision is to expand the boundaries of innovation through the
-            optimal utilization of technology, culminating in the creation of a
-            cutting-edge Martian Rover. This advanced technology will serve as
-            a catalyst, inspiring and fueling the spirit of space exploration.
+          At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.
           </div>
         </div>
       </div>
