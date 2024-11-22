f9fefe99818b7fabb7897e73e52d47bc9ecae40e
Agrim <agrimtawani139@gmail.com>
2024-11-22 23:02:52 +0530

    added about irc

---
+++ src/components/AboutIrc.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AboutIrc = () => {
+  return (
+    <div className='flex flex-col gap-3 justify-center items-center'>
+        <div className='text-white font-meth component_title -mt-[11%] font-bold'>About IRC 2025</div>
+
+        <div className='text-white font-poppins w-[80%] text-center text-xl block'>
+        The International Rover Challenge 2025 is a premier global competition where teams from universities and research institutions around the world design, build, and deploy autonomous rovers to tackle a variety of challenging tasks. The competition simulates real-world scenarios that require cutting-edge technology in robotics, navigation, and AI. Teams are tasked with designing rovers capable of performing tasks such as terrain navigation, obstacle avoidance, data collection, and remote operations. IRC encourages innovation, teamwork, and problem-solving in robotics, providing participants with an unparalleled opportunity to showcase their engineering skills while contributing to the future of space exploration, planetary rovers, and autonomous systems. Join us in this exciting race to push the limits of rover technology!      
+        </div>
+    </div>
+  )
+}
+
+export default AboutIrc
--- src/components/IRC25.jsx
+++ src/components/IRC25.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Navbar from './Navbar'
+import AboutIrc from './AboutIrc'
 
 const IRC25 = () => {
   return (
-    <div className='h-screen'>
-        <Navbar />
+    <div className="h-screen flex flex-col justify-center items-center">
+        <div className="">
+            <Navbar />
+        </div>
+        <div className="">
+            <AboutIrc />
+        </div>
     </div>
+
   )
 }
 
