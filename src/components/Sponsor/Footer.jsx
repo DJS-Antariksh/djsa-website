f94bc885a3540ab92b64d0c9436036e4d65dbb02
Anay Shah <anayshah10@gmail.com>
2025-12-02 01:12:31 +0530

    footer + sponsors + tweaks

--- src/components/Sponsor/Footer.jsx
+++ src/components/Sponsor/Footer.jsx
@@ -4,11 +4,17 @@ import bg from "../../assets/footerbg.png";
 import { Link } from "react-scroll";
 import {FaXTwitter} from 'react-icons/fa6';
 
-
-
 const Footer = () => (
-  <div className="relative">
-    <img src={bg} alt="footerbg" className="absolute z-0 object-cover w-full h-[26rem]" />
+  <div className="relative w-full">
+    <img 
+      src={bg} 
+      alt="footerbg" 
+      className="absolute z-0 object-cover w-full h-full opacity-70"
+      style={{
+        maskImage: 'linear-gradient(to bottom, transparent, white)',
+        WebkitMaskImage: 'linear-gradient(to bottom, transparent, white)'
+      }}
+    />
     <section className="relative flex flex-col items-center justify-center py-6 sm:py-16 z-99">
       <div className="relative z-10 flex flex-col md:flex-row items-center w-full mb-8 justify-center">
 
@@ -19,83 +25,11 @@ const Footer = () => (
           </p>
         </div>
 
-        {/* Hide the Useful Links section on small screens */}
-        {/* <div className="hidden sm:flex flex-col items-center md:items-start mt-8 md:mt-0">
-          <h4 className="font-poppins font-normal text-[18px] leading-[27px] text-white mb-4">Useful Links</h4>
-          <ul className="list-none font-poppins text-[16px] leading-[24px] text-white">
-            <li className="mt-2 md:mt-4">
-              <Link to="/" spy={true} smooth={true} duration={500} className="text-white cursor-pointer hover:text-gray-500">
-                Home
-              </Link>
-            </li>
-            <li className="mt-4">
-              <Link
-                to="about"
-                spy={true}
-                smooth={true}
-                duration={500}
-                className="text-white cursor-pointer font-extralight font-poppins hover:text-gray-500 hover:decoration-white/30"
-              >
-                About Us
-              </Link>
-            </li>
-            <li className="mt-4">
-              <Link
-                to="achievements" 
-                spy={true}
-                smooth={true}
-                duration={500}
-                className="text-white cursor-pointer font-extralight font-poppins hover:text-gray-500 hover:decoration-white/30"
-              >
-                Achievements
-              </Link>
-            </li>
-            <li className="mt-4">
-              <Link
-                to="contactUs"
-                spy={true}
-                smooth={true}
-                duration={500}
-                className="text-white cursor-pointer font-extralight font-poppins hover:text-gray-500 hover:decoration-white/30"
-              >
-                Contact Us
-              </Link>
-            </li>
-            <li className="mt-4">
-              <Link
-                to="sponsor"
-                spy={true}
-                smooth={true}
-                duration={500}
-                className="text-white cursor-pointer font-extralight font-poppins hover:text-gray-500 hover:decoration-white/30"
-              >
-                Sponsors
-              </Link>
-            </li>
-          </ul>
-        </div> */}
-
-        {/* Hide the About the Team section on small screens */}
-        {/* <div className="hidden sm:flex flex-col items-center md:items-start mt-4 md:mt-0">
-          <h4 className="font-poppins font-normal text-[18px] leading-[27px] text-white mb-4">About the Team</h4>
-          <ul className="list-none font-poppins text-[16px] leading-[24px] text-white">
-            <li className="mt-2 md:mt-4 hover:text-secondary">
-              <Link to="rover" spy={true} smooth={true} duration={500} className="text-white cursor-pointer hover:text-gray-500">
-                Our Rover
-              </Link>
-            </li>
-           
-          </ul>
-        </div> */}
-
-<div className="flex flex-col items-center md:items-center mt-9 md:mt-0 md:pt-57 lg:ml-20">
-
-
-          {/* <h4 className="font-poppins font-normal text-[18px] leading-[27px] text-white mb-4">Follow Us On</h4> */}
+        {/* Social Media Links */}
+        <div className="flex flex-col items-center md:items-center mt-9 md:mt-0 md:pt-57 lg:ml-20">
           <ul className="list-none flex flex-row text-white">
-          <li className="m-2 hover:text-secondary">
-              <a href="https://instagram.com/djs_antariksh?igshid=MXQxZ2thMXZob3poYQ==
-              " target="_blank" rel="noopener noreferrer">
+            <li className="m-2 hover:text-secondary">
+              <a href="https://instagram.com/djs_antariksh?igshid=MXQxZ2thMXZob3poYQ==" target="_blank" rel="noopener noreferrer">
                 <AiFillInstagram className="w-[30px] h-[30px]" />
               </a>
             </li>
@@ -123,11 +57,11 @@ const Footer = () => (
         </div>
       </div>
 
-      <div className="w-[100vw] mx-[10vw] flex flex-row justify-center md:justify-start p-0 pt-0 border-t-[1px] border-t-[#3F3E45]">
-  <p className="font-poppins font-thin text-center md:text-left text-[18px] leading-[27px] text-white md:pl-[28vw]">
-    Copyright Ⓒ 2024 DJS Antariksh
-  </p>
-</div>
+      <div className="w-full flex flex-row justify-center p-0 pt-0">
+        <p className="font-poppins font-thin text-center text-[18px] leading-[27px] text-white">
+          Copyright Ⓒ 2024 DJS Antariksh
+        </p>
+      </div>
     </section>
   </div>
 );
--- src/components/Sponsor/Sponsor.jsx
+++ src/components/Sponsor/Sponsor.jsx
@@ -1,52 +1,30 @@
 import React from "react";
-import solidworks from "../../assets/solidworks.png";
-// import WOL from "../../assets/wol.png";
-import PA from "../../assets/progressive.webp";
-import alpha from "../../assets/alpa.png";
-import ANYSYS from "../../assets/ansys-logo.jpg";
-import spectra from "../../assets/spectra.jpeg"
-import tesseract from "../../assets/tesseract.png"
-// import seshasai from "../../assets/SESHASAI-LOGO.png"
 
 const Sponsor = () => {
+  const sponsors = [
+    "/sponsors2026/2.png",
+    "/sponsors2026/3.png",
+    "/sponsors2026/4.png",
+    "/sponsors2026/5.png",
+    "/sponsors2026/6.png",
+    "/sponsors2026/7.png",
+  ];
+
   return (
     <div id="sponsor">
       <div className="mt-[-4rem]">
         <div className="flex justify-center mt-16 mb-2 text-3xl text-white font-meth component_title">
           Sponsors
         </div>
-        <div className="flex flex-wrap justify-center">
-
-          <img
-            className="m-12 w-[20%] h-[5rem] sm:h-[10rem] rounded-[5%] transition-transform duration-300 hover:scale-110"
-            src={solidworks}
-            alt="sponsor img"
-          />
-
-          <img
-            className="m-12 w-[20%] h-[5rem] sm:h-[10rem] rounded-[5%] transition-transform duration-300 hover:scale-110"
-            src={alpha}
-            alt="sponsor img"
-          />
-
-          <img
-            className="m-12 w-[20%] h-[5rem] sm:h-[10rem] rounded-[5%] transition-transform duration-300 hover:scale-110"
-            src={ANYSYS}
-            alt="sponsor img"
-          />
-
-          <img
-            className="m-12 w-[20%] h-[5rem] sm:h-[10rem] rounded-[5%] transition-transform duration-300 hover:scale-110"
-            src={spectra}
-            alt="sponsor img"
-          />
-
-          <img
-            className="m-12 w-[20%] h-[5rem] sm:h-[10rem] rounded-[5%] transition-transform duration-300 hover:scale-110"
-            src={tesseract}
-            alt="sponsor img"
-          />
-
+        <div className="flex flex-wrap justify-center items-center">
+          {sponsors.map((src, index) => (
+            <img
+              key={index}
+              className="m-12 w-[20%] h-[5rem] sm:h-[10rem] object-contain rounded-[5%] transition-all duration-300 hover:scale-110 drop-shadow-[0_0_5px_rgba(255,255,255,0.5)] hover:drop-shadow-[0_0_15px_rgba(255,255,255,0.9)]"
+              src={src}
+              alt={`sponsor ${index + 1}`}
+            />
+          ))}
         </div>
       </div>
     </div>
